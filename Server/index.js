52ecc8e8364344a05d00d30d325246d19afcaae3
Usama Ahmad <usamaahmad@desygner.com>
2025-03-25 22:01:24 +1000

    Adding env

--- Server/index.js
+++ Server/index.js
@@ -10,13 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 mongoose
-  .connect(
-    "mongodb+srv://saddambusiness247:M5kfTOZeyLAdkXjg@cluster0.dlpma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
 
